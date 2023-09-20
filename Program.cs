9efde551c53b4b7c247d281638f0729b773c2e72
null <null>
2023-09-20 12:27:43 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Arreglos;
+using System;
+
+
+namespace aRREGLOSuNIDIMENSIONALES_g2_GRAJ
+{
+    internal class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Arreglo.Asignar();
+            Arreglo.Imprimir();
+            Console.WriteLine("\n\n Inverso:");
+            Arreglo.Invetir();
+            Arreglo.Imprimir();
+            Console.ReadLine();
+
+        }
+    }
+}
